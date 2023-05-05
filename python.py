83c231401bf5a5819975aaf6ac728443f01642ac
Haroon Mahmoud <122131858+Coolrooney10@users.noreply.github.com>
2023-05-05 14:18:08 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,6 @@
+import mysql.connector
+connection = mysql.connector.connect(user = 'root', database = 'bank', password = 'password')
+cursor = connection.cursor()
 program = True
 def numCheck(num):
   num_is_int = False
@@ -7,16 +10,17 @@ def numCheck(num):
     #If the input is a number put num_is_int as True
     num_is_int = True
   except ValueError:
-    print("\nTry again using a whole number")
+    print("\nTry again using a whole number ")
     # If the input isn't a number put num_is_int as False
     num_is_int = False
   # Return num_is_int
   return num_is_int
-print('Hello, welcome to H&M Bank of Austin')
+print('Hello, welcome to Express Bank of Austin ')
 
 
 def createAccount():
-  user = input("Please enter your first name and last name: ")
+  userFirstInput = input("Please enter your first name ")
+  userLastInput = input("Please enter your last name ")
   passMatch = False
 
     # Print pins rules
@@ -40,45 +44,47 @@ def createAccount():
           print("Pins do not match. \nTry Again")
           passMatch = False
     else:
-      print("Pin is too short try again")
+      print("Pin is too short try again ")
       passMatch = False
+  userPin = userInputPin
+  addAccount(userPin, userFirstInput, userLastInput, amount)
 
 createAccount()
 
 amount = 0
-
 while program == True:
-  
+
+    
 
   print(' 1.Deposit\n 2.Withdraw\n 3.modify account\n 4.Check Balance\n 5.Delete Account\n 6.Exit')
-  options = input("Type a number corresponding to the use (1-6):  ")
-  if "1" in options:
-    addedMoney = input("How much would you like to deposit")
+  options = input("Type a number corresponding to the use (1-6) ")
+  if '1' in options:
+    addedMoney = input("How much would you like to deposit ")
     newAmount = int(amount) + int(addedMoney)
     amount = newAmount
     print(f"You deposited {addedMoney}, and your new total is {newAmount}")
-  elif "2" in options:
-    moneyTaken = input("How much would you like to withdraw")
+  elif '2' in options:
+    moneyTaken = input("How much would you like to withdraw ")
     newAmount = int(amount) - int(moneyTaken)
     amount = newAmount
     print(f"You withdrew {moneyTaken}, and your new total is {newAmount}")
-  elif "3" in options:
+  elif '3' in options:
     createAccount()
-  elif "4" in options:
+  elif '4' in options:
     print(amount)
-  elif "5" in options:
-    delete = input("Are you sure? you won\'t be ableto get your account back.'")
+  elif '5' in options:
+    delete = input("Are you sure? You will not be able to get this account back ")
     if delete == 'yes':
-      print('Well, it\'s been fun T_T') 
+      print('Alright, it is your choice T-T ')
       exit()
     else :
-      print("Glad you decided to stay with us!")
+      print("Glad you decided to stay with us! ")
   elif '6' in options:
-    print('Bye!')
     exit()
   else:
-    print('Sorry, I am not able to comprehend that, please try again')
+    print('Sorry, I am not able to comprehend that, please try again ')
 
-
-if program == False:
-    exit()
+def addAccount( userPin, userFirstInput, userLastInput, amount):
+    addData = (f"INSERT INTO `bank`.`bank_info` (`Account_Pin`, `First_Name`, `Last_Name`, `balance`) VALUES ({userPin}, '{userFirstInput}', '{userLastInput}', '{amount}', '0")
+    cursor.execute(addData)
+    connection.commit()
